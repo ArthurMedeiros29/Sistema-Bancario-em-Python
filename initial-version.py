6cfc0d920c64f0f842178f88f5ed562fbd3ea0a2
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2025-05-22 14:42:59 -0300

    add challenge 2

--- initial-version.py
+++ initial-version.py
@@ -1,11 +1,20 @@
+from datetime import datetime
+
 saldo = 0
 limite = 500
 numero_saques = 0
 limite_saques = 3
+limite_transacoes = 10
+
 total_depositado = 0
 total_sacado = 0
+transacoes = []
 
 while True:
+    if len(transacoes) >= limite_transacoes:
+        print("Limite de 10 transacoes diarias atingido. AS operacoes estao bloqueadas")
+        break
+
     print("""
     [D] Depositar
     [S] Sacar
@@ -20,6 +29,8 @@ while True:
         if deposito > 0:
             saldo += deposito
             total_depositado += deposito
+            data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            transacoes.append(("Deposito", deposito, data_hora))
         else:
             print("Operação falhou! O valor informado é inválido.")
     
@@ -36,11 +47,18 @@ while True:
             saldo -= sacar
             total_sacado += sacar
             numero_saques += 1
+            data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            transacoes.append(("Saque", sacar, data_hora))
         else:
             print("Operação falhou! O valor informado é inválido.")
     
     elif opcao == "E":
         print("\n=============== EXTRATO ===============")
+        if transacoes:
+            for tipo, valor, data_hora in transacoes:
+                print(f"{data_hora} - {tipo}: R${valor:.2f}")
+        else:
+            print("Não foram realizadas movimentacoes")
         print(f"Total depositado: R${total_depositado:.2f}")
         print(f"Total sacado:     R${total_sacado:.2f}")
         print(f"Saldo atual:      R${saldo:.2f}")
